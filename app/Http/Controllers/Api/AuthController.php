f6b024c929d72fcb7c2d24fcfa5dacef49e21455
kleefi <decharvi.charles@gmail.com>
2025-07-30 14:24:04 +0700

    auth function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,47 +3,75 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AuthResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function register(Request $request)
     {
-        //
-    }
+        $validated = $request->validate([
+            'name' => 'string|required',
+            'email' => 'string|required|email',
+            'password' => 'string|required|min:8',
+        ]);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        if (!$validated) {
+            return response()->json(
+                [
+                    "status" > false,
+                    "message" => "failed to create user",
+                ]
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+            );
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return response()->json([
+            'status' => true,
+            'message' => 'user created',
+            'data' => new AuthResource($user)
+        ], 201);
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function login(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'string|required|email',
+            'password' => 'string|required'
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                "status" => false,
+                "message" => "password or email not match"
+            ], 422);
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            "status" => true,
+            "message" => "login succedd",
+            "token" => $token
+        ], 200);
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function user(Request $request)
     {
-        //
+        $user = $request->user();
+        return response()->json($user);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'status' => true,
+            'message' => 'user logged'
+        ], 200);
     }
 }
